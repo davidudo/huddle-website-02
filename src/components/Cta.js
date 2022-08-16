475708d41c751381b1d71979f7ccc31ecc47cb83
David Udo <udodavid46.ud@gmail.com>
2022-08-16 08:35:33 +0100

    Fixed CTA responsiveness

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,8 +1,10 @@
 function Cta() {
   return (
-    <div className="cta-container">
-      <h2>Ready To Build Your Community?</h2>
-      <button>Get Started For Free</button>
+    <div className="container">
+      <div className="cta-container">
+        <h2>Ready To Build Your Community?</h2>
+        <button>Get Started For Free</button>
+      </div>
     </div>
   );
 }
