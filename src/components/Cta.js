ef40d4a9b90b2d169adcb1efdefaf5acc3555431
David Udo <udodavid46.ud@gmail.com>
2022-08-16 17:18:27 +0100

    Fixed Info Card responsiveness

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,6 +1,6 @@
 function Cta() {
   return (
-    <div className="container">
+    <div>
       <div className="cta-container">
         <h2>Ready To Build Your Community?</h2>
         <button>Get Started For Free</button>
